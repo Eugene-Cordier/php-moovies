d66d2426ffef2b59a47ccdea0170e451d9bc1577
Enzo Rolin <enzo.rolin@etudiant.univ-reims.fr>
2023-06-12 18:18:18 +0200

    Ajout de l'image par défaut et création de la page d'acceuil.

--- public/index.php
+++ public/index.php
@@ -1,11 +1,31 @@
 <?php
 
+use Entity\Collection\Movie_collection;
 use Entity\Movie;
 use Html\WebPage;
 
+$tab= Movie_collection::findAllMovie();
 $webPage= new WebPage();
-$film = Movie::findById(108);
-$webPage->appendContent($film->getTitle());
+$webPage->setTitle('Films');
+$webPage->appendContent(<<<HTML
+    <div class="content">
+HTML);
+
+foreach ($tab as $elt) {
+    $cover="image.php?imageId={$elt->getPosterId()}";
+    if($elt->getPosterId()==null) {
+        $cover="img/defaultcover.png";
+    }
+    $webPage->appendContent(<<<HTML
+        <div class="film">
+            <a href="movies.php?moviesId={$elt->getId()}">
+                <div class="poster"><img src="$cover"></div>
+                <div class="titre">{$elt->getTitle()}</div>
+            </a>
+        </div>
+        HTML);
+}
+$webPage->appendContent(<<<HTML
+        <footer class='footer'>Derniere modification : {$webPage->getLastModification()}</footer>
+    HTML);
 echo $webPage->toHtml();
-
-
