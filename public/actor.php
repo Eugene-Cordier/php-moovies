0a47ae12d9fb0f7972ead21542071649080f9483
Enzo Rolin <enzo.rolin@etudiant.univ-reims.fr>
2023-06-13 15:24:35 +0200

    Création de la troisième page actor.php .

---
+++ public/actor.php
@@ -0,0 +1,63 @@
+<?php
+
+
+declare(strict_types=1);
+
+use Entity\Cast;
+use Entity\Collection\Movie_collection;
+use Entity\People;
+use Html\WebPage;
+
+$actorId=(int)$_GET["ActorId"];
+$actor=People::findById($actorId);
+$webPage= new WebPage($actor->getName());
+$webPage->appendCssurl("css/style3.css");
+$webPage->appendContent("<header> Films-{$actor->getName()}</header>");
+$cover="image.php?ImageId={$actor->getAvatarId()}&Type=People";
+$webPage->appendContent(<<<HTML
+<div class="content">
+    <div class="actor">
+        <img class="imgmovie" src=$cover>
+        <div class="infoactor">
+            <p>{$actor->getName()}</p>
+            <p>{$actor->getPlaceOfBirth()}</p>
+            <div class="datebirthdead">
+                <p class="birthday"> {$actor->getbirthday()} </p>
+                <P class="deathday">{$actor->getDeathday()}</P>
+            </div>
+            <p class="biography"> {$actor->getBiography()}</p>
+        </div>
+    </div>
+
+HTML);
+
+$movieList=Movie_collection::findMoviesByPeopleId($actorId);
+foreach($movieList as $movie) {
+    $poster="image.php?ImageId={$movie->getPosterId()}";
+    $title=$movie->getTitle();
+    $date=$movie->getReleaseDate();
+    $cast=Cast::getCast($movie->getId(), $actorId);
+    $role=$cast->getRole();
+    $webPage->appendContent(<<<HTML
+    <a href="movies.php?moviesId={$movie->getId()}"
+    <div class="movie">
+        <img src="$poster">
+        <div class="infomovie">
+            <div class="titledate">
+                <p class="title">$title</p>
+                <p class="date">$date</p>
+            </div>
+            <p class="role">$role</p>
+        </div>
+    </div>
+    </a>
+
+HTML);
+}
+
+$webPage->appendContent(<<<HTML
+        </div>
+        <footer class='footer'>Derniere modification : {$webPage->getLastModification()}</footer>
+    HTML);
+
+echo $webPage->toHtml();
