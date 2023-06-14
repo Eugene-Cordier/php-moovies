29c6b6f737c62b48586b9a3d3ba6d3d3c49e2549
cord0048 <eugene.cordier@etudiant.univ-reims.fr>
2023-06-14 11:36:36 +0200

    ajout du programme Movie.-delete.php pour delete un film.

---
+++ public/admin/movie-delete.php
@@ -0,0 +1,24 @@
+<?php
+
+
+declare(strict_types=1);
+
+use Entity\Movie;
+use Entity\Exception\EntityNotFoundException;
+use Exception\ParameterException;
+
+try {
+    if(!(isset($_GET['movieId'])) || !(ctype_digit($_GET['movieId']))) {
+        throw new ParameterException("parametre non valide");
+    } else {
+        $movie= Movie::findById((int)$_GET['movieId']);
+        $movie->delete();
+        header("location:../index.php", true,302);
+    }
+} catch (ParameterException) {
+    http_response_code(400);
+} catch (EntityNotFoundException) {
+    http_response_code(404);
+} catch (Exception) {
+    http_response_code(500);
+}
